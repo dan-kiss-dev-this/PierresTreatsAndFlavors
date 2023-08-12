383d99ed3b2094998a044e9e63b18849d004560b
Daniel Kiss <flowfast47@gmail.com>
2023-08-11 18:14:36 -0700

    redirect user when they create account or login

--- PierresTreatsAndFlavors/Controllers/AccountUserController.cs
+++ PierresTreatsAndFlavors/Controllers/AccountUserController.cs
@@ -76,7 +76,7 @@ namespace PierresTreatsAndFlavors.Controllers
         Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
         if (result.Succeeded)
         {
-          return RedirectToAction("Index");
+          return RedirectToAction("Index", "Home");
         }
         else
         {
